b3ea3824245058a662b513979127bfea83f3dbb7
1exoticrailfan <utkarsh.25209@ee.du.ac.in>
2026-01-16 01:50:32 +0530

    first commit

---
+++ Ass02/1.py
@@ -0,0 +1,16 @@
+"""
+Algorithm to arrange clothes in an almirah
+
+1. Start
+2. Take number of shelves as input (n)
+3. Create categories of clothes
+   - Formal
+   - Casual
+   - Traditional
+4. Assign one or more shelves to each category
+5. Place frequently used clothes on middle shelves
+6. Place less used clothes on top shelves
+7. Arrange clothes neatly in each shelf
+8. Label each shelf with category name
+9. End
+"""
---
+++ Ass02/2.py
@@ -0,0 +1,28 @@
+"""
+Algorithm to pack clothes in a luggage bag
+
+1. Start
+
+2. Select destination and trip duration (7 days)
+
+3. List daily activities
+   - Sightseeing
+   - Hiking
+   - Formal dinner
+
+4. Select clothes according to activities
+
+5. Fold heavy clothes first and place at bottom
+
+6. Place light clothes on top
+
+7. Keep frequently needed items on top
+
+8. Pack delicate clothes separately
+
+9. Use remaining space for accessories
+
+10. Close the bag properly
+
+11. End
+"""
